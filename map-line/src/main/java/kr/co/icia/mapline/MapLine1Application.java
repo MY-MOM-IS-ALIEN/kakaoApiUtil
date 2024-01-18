ea25dd6f4b3d6987ffb665f015262d007a8ca7ff
MY-MON-IS-ALIEN <alsdn5471@naver.com>
2024-01-18 16:52:50 +0900

    map-line

---
+++ map-line/src/main/java/kr/co/icia/mapline/MapLine1Application.java
@@ -0,0 +1,13 @@
+package kr.co.icia.mapline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapLine1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapLine1Application.class, args);
+	}
+
+}
---
+++ map-line/src/test/java/kr/co/icia/mapline/MapLine1ApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.icia.mapline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapLine1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
